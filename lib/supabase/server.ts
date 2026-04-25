71d3344b9bf065c2f619fb9fd49ed895a3b9484e
jjohngrey <john1grey9@gmail.com>
2026-04-24 18:59:35 -0600

    add cookie type for server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,6 @@
-import { createServerClient } from "@supabase/ssr";
+import { CookieOptions, createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+
 
 /**
  * Supabase client for Server Components, Route Handlers, and Server Actions.
@@ -24,7 +25,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options);
